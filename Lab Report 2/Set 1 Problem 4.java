5e1f8a7ae343e772a001edd53cbb19fdac3c777d
Rahim1068 <rahim101403@gmail.com>
2026-03-19 23:16:34 +0600

    Create Set 1 Problem 4.java

---
+++ Lab Report 2/Set 1 Problem 4.java
@@ -0,0 +1,32 @@
+package sd_lab.report.pkg2_set.pkg1.problem.pkg4;
+
+import java.util.Scanner;
+
+public class SD_LabReport2_Set1Problem4 {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        
+        int max = a;   // assume first is max
+        
+        if(b > max){
+            max = b;
+        }
+        
+        if(c > max){
+            max = c;
+        }
+        
+        System.out.println("Largest number is: " + max);
+        
+    }
+}
