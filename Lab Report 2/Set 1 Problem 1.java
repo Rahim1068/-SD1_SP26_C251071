f67ab58b276eeda959f496e14dcc564d422c5f69
Rahim1068 <rahim101403@gmail.com>
2026-03-19 22:42:47 +0600

    Create Set 1 Problem 1.java

---
+++ Lab Report 2/Set 1 Problem 1.java
@@ -0,0 +1,8 @@
+package sd_lab.report.pkg2_set.pkg1_problem.pkg1_c251071;
+public class SD_LabReport2_SET1_Problem1_C251071 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+    }
+    
+}
