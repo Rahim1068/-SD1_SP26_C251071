1d5e1d55e45da35e8d54351c5ddcacf91896530e
Rahim1068 <rahim101403@gmail.com>
2026-03-20 00:03:44 +0600

    Create Set 1 Problem 6.java

---
+++ Lab Report 2/Set 1 Problem 6.java
@@ -0,0 +1,17 @@
+package sd_lab.report.pkg2_set.pkg1_problem.pkg6_c251071;
+
+import java.util.Scanner;
+
+public class SD_LabReport2_Set1_Problem6_C251071 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
