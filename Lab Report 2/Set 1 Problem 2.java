5b86335f632917e6c172ca4ab737f6754173038d
Rahim1068 <rahim101403@gmail.com>
2026-03-19 22:50:47 +0600

    Create Set 1 Problem 2.java

---
+++ Lab Report 2/Set 1 Problem 2.java
@@ -0,0 +1,16 @@
+package sd_labreport2_set1_problem2_c251071;
+
+import java.util.Scanner;
+
+public class SD_LabReport2_SET1_Problem2_C251071 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Hello, " + name + "!");
+      
+    }
+}
