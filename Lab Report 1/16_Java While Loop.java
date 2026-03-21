488eaa3721031fd4f92c90714f687ad8c32f9b97
Rahim1068 <rahim101403@gmail.com>
2026-03-22 00:03:36 +0600

    Create 16_Java While Loop.java

---
+++ Lab Report 1/16_Java While Loop.java
@@ -0,0 +1,18 @@
+package sd_lab_report_1_java_while_loop_c251071;
+
+public class SD_Lab_Report_1_Java_While_Loop_C251071 {
+
+    public static void main(String[] args) {
+        
+        int countdown = 7;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+        
+    }
+    
+}
