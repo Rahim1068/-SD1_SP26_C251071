bd10a3553ab7c23ad61b1fc34b97af9bea5320bf
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:24:09 +0600

    Create 9_Java Type Casting.java

---
+++ Lab Report 1/9_Java Type Casting.java
@@ -0,0 +1,13 @@
+package sd_lab_report_1_java_type_casting_c251071;
+
+public class SD_Lab_Report_1_Java_Type_Casting_C251071 {
+    public static void main(String[] args) {
+        
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        
+    }
+}
