db7412e2dadca58d66f02d3e5b63ddead87056d2
Rahim1068 <rahim101403@gmail.com>
2026-03-21 23:40:47 +0600

    Create 13_Java Booleans.java

---
+++ Lab Report 1/13_Java Booleans.java
@@ -0,0 +1,14 @@
+package sd_lab_report_1_java_booleans_c251071;
+
+public class SD_Lab_Report_1_Java_Booleans_C251071 {
+
+    public static void main(String[] args) {
+        
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;    
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        
+    }
+    
+}
