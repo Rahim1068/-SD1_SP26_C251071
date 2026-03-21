c7be03c1ba23f91252e7d837dd01910162ce898f
Rahim1068 <rahim101403@gmail.com>
2026-03-21 23:47:31 +0600

    Create 14_Java If...Else.java

---
+++ Lab Report 1/14_Java If...Else.java
@@ -0,0 +1,17 @@
+package sd_lab_report_1_java_if.else_c251071;
+
+public class SD_Lab_Report_1_Java_IfElse_C251071 {
+
+    public static void main(String[] args) {
+        
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Allowed");
+        } else {
+            System.out.println("Not allowed");
+        }
+        
+    }
+    
+}
