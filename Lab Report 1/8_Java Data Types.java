18548aeb0bd480701484fbd9ef466ac208cd5622
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:17:55 +0600

    Create 8_Java Data Types.java

---
+++ Lab Report 1/8_Java Data Types.java
@@ -0,0 +1,19 @@
+package sd_lab_report_1_java_data_types_c251071;
+
+public class SD_Lab_Report_1_Java_Data_Types_C251071 {
+    public static void main(String[] args) {
+        
+        int myNum = 251071;               
+        float myFloatNum = 3.99f;    
+        char myLetter = 'R';         
+        boolean myBool = true;       
+        String myText = "Hello";        
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+        
+    }
+}
