85bb73a8a236dc2ed27d55ff7e8ed590f74ba913
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:53:34 +0600

    Create 12_Java Math.java

---
+++ Lab Report 1/12_Java Math.java
@@ -0,0 +1,9 @@
+package sd_lab_report_1_java_math_c251071;
+
+public class SD_Lab_Report_1_Java_Math_C251071 {
+    public static void main(String[] args) {
+        
+        System.out.println(Math.sqrt(64));
+        
+    }
+}
