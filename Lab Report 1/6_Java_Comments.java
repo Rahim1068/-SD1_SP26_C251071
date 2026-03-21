c4165223a1cc5cf2b8acb1fd91e435dda103120e
Rahim1068 <rahim101403@gmail.com>
2026-03-21 21:52:04 +0600

    Create 6_Java_Comments.java

---
+++ Lab Report 1/6_Java_Comments.java
@@ -0,0 +1,11 @@
+package sd_lab_report_1_java_comments_c251071;
+
+public class SD_Lab_Report_1_Java_Comments_C251071 {
+    public static void main(String[] args) {
+        
+        /* The code below will print the words Hello World
+           to the screen, and it is amazing */
+        System.out.println("Hello IIUC");
+        
+    }
+}
