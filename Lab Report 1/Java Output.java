3695f42b6c283faed752aab9647c78fa70c067f8
Rahim1068 <rahim101403@gmail.com>
2026-03-21 21:06:08 +0600

    Create 5_Java Output.java

---
+++ Lab Report 1/Java Output.java
@@ -0,0 +1,10 @@
+package sd_lab_report_1_java_output_c251071;
+
+public class SD_Lab_Report_1_Java_Output_C251071 {
+    public static void main(String[] args) {
+        
+        System.out.println("Rahim");
+        System.out.println(21);
+        
+    }
+}
