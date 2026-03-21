f521295407879ba5bc822cbdfb6ddd70a61980b8
Rahim1068 <rahim101403@gmail.com>
2026-03-22 00:08:36 +0600

    Create 17_Java For Loop.java

---
+++ Lab Report 1/17_Java For Loop.java
@@ -0,0 +1,13 @@
+package sd_lab_report_1_java_for_loop_c251071;
+
+public class SD_Lab_Report_1_Java_For_Loop_C251071 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 12; i = i + 2) {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
