1df14d2bd1bc1c6b6d6e3f81c4e6115761084708
Rahim1068 <rahim101403@gmail.com>
2026-03-21 20:20:20 +0600

    Create Java Intro.java

---
+++ Lab Report 1/Java Intro.java
@@ -0,0 +1,9 @@
+package sd_lab_report_1_java_intro_c251071;
+
+public class SD_Lab_Report_1_Java_Intro_C251071 {
+    public static void main(String[] args) {
+        String name = "Rahim Sarker";
+        System.out.println("Hello " + name);
+        
+    }
+}
