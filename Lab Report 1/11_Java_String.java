351c4331fd3f2b5dada7056e04ff22fbab093ea8
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:47:37 +0600

    Create 11_Java_String.java

---
+++ Lab Report 1/11_Java_String.java
@@ -0,0 +1,11 @@
+package sd_lab_report_1_java_strings_c251071;
+
+public class SD_Lab_Report_1_Java_Strings_C251071 {
+    public static void main(String[] args) {
+        
+        String name = "Rahim";
+        int age = 23;
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+        
+    }
+}
