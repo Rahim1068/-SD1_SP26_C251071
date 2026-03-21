4ded9ac428a374c5367240e8b6aee043ebba52dd
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:03:54 +0600

    Create 7_Java Variables.java

---
+++ 7_Java Variables.java
@@ -0,0 +1,11 @@
+package sd_lab_report_1_java_variables_c251071;
+
+public class SD_Lab_Report_1_Java_Variables_C251071 {
+    public static void main(String[] args) {
+        
+        int myNum;
+        myNum = 251071;
+        System.out.println(myNum);
+        
+    }
+}
