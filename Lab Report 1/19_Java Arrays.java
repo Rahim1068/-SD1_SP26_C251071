9bf46b29cabde1afabe0278a91cfa538be7bbbec
Rahim1068 <rahim101403@gmail.com>
2026-03-22 00:17:04 +0600

    Create 19_Java Arrays.java

---
+++ Lab Report 1/19_Java Arrays.java
@@ -0,0 +1,12 @@
+package sd_lab_report_1_java_arrays_c251071;
+
+public class SD_Lab_Report_1_Java_Arrays_C251071 {
+
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+        
+    }
+    
+}
