6ae9f7e8d55503daca734fa8ef713218f6981e12
Rahim1068 <rahim101403@gmail.com>
2026-03-21 22:36:32 +0600

    Create 10_Java Operators.java

---
+++ Lab Report 1/10_Java Operators.java
@@ -0,0 +1,22 @@
+package sd_lab_report_1_java_operators_c251071;
+
+public class SD_Lab_Report_1_Java_Operators_C251071 {
+    public static void main(String[] args) {
+        
+        int x = 13;
+        int y = 4;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        int z = 8;
+        ++z;
+        System.out.println(z);
+        --z;
+        System.out.println(z);
+        
+    }
+}
