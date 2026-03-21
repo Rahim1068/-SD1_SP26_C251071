dbcfb06522b97c42fedd290a7059572579972985
Rahim1068 <rahim101403@gmail.com>
2026-03-21 20:36:59 +0600

    Create Java Get Started.java

---
+++ Lab Report 1/Java Get Started.java
@@ -0,0 +1,9 @@
+package sd_lab_report_1_java_get_started_c251071;
+
+public class SD_Lab_Report_1_Java_Get_Started_C251071 {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        
+    }
+}
